7c02f047d2ac846a7d005808e7ce27220a33d445
jrissler <james@jamesrissler.com>
2010-12-29 09:02:52 -0500

    Updated javascript for jquery modal + calendar.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
 
-$('.datepicker').datetimepicker({
-	ampm: true
-});
+  $('.datepicker').datetimepicker({
+  	ampm: false,
+  	timeFormat: 'hh:mm:ss',
+  	dateFormat: 'yy-mm-dd'
+  });
+
+  $(".event_dialog").click(function() {
+    date = $(this).attr('year') + '-' + $(this).attr('month') + '-' + $(this).attr('day')
+    $('#event_start_at').val(date);
+    $('#event_end_at').val(date);
+    $('#new_event_dialog').dialog('open');
+    return false;
+  });
+
+  $( "#new_event_dialog" ).dialog({
+  	autoOpen: false,
+  	height: 300,
+  	width: 350,
+  	modal: true,
+  	title: "Add a New Event"
+  });
+
 });
