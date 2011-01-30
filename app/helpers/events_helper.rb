ef3e639825df516d8946a6c58511ba51b62dbc4d
James Rissler <risslerjames@gmail.com>
2011-01-30 07:40:14 -0500

    Override some of the event_helpers methods.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,3 +1,19 @@
 module EventsHelper
-
+  
+  def calendar_month(event)
+    content_tag :p, :class => "small_month_name" do
+      month(event.start_at)
+    end
+  end
+  
+  def calendar_day(event)
+    content_tag :p, :class => "small_month_dates" do
+      day(event.start_at) + qualifies_for_later(event)
+    end
+  end
+  
+  def qualifies_for_later(event)
+    event.end_at && event.end_at > event.start_at ? "- #{day(event.end_at)}" : ""
+  end
+  
 end
